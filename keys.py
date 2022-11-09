84510e36a8c57540540ad4a1d09d3849b3e7d959
Nick Goko <61675328+NickGoko@users.noreply.github.com>
2022-11-09 21:39:16 +0300

    delete keys.py

--- keys.py
+++
@@ -1,5 +0,0 @@
-NT_APP_KEY = 'c7874081a4007df7b6042aba9ecb4cc0'
-NT_APP_ID = '9622191f'
-
-SHEET_ENDPOINT = "https://api.sheety.co/39e2a8a4a6946ffd8bb9e5dfb1742f8a/myWorkouts/workouts"
-TOKEN = "DrNzScq9!2qjUkQJ"
